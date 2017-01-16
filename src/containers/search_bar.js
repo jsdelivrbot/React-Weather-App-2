213e43a67890c8cdddb1f63e48c64981de5bd854
Jeff Stock <jeffcstock@gmail.com>
2017-01-15 21:51:30 -0800

    Add weather reducer

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -9,6 +9,7 @@ class SearchBar extends Component {
 
     this.state = { term: '' };
     this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
   }
 
   onInputChange(event) {
@@ -18,7 +19,8 @@ class SearchBar extends Component {
   onFormSubmit(event) {
     event.preventDefault();
     // We need to go and fetch weather data.
-    
+    this.props.fetchWeather(this.state.term);
+    this.setState({ term: '' });
 
   }
 
