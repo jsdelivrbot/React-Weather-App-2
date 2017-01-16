eae5a4bdbc7643beb77b0aaa857a8a30f96a4e8e
Jeff Stock <jeffcstock@gmail.com>
2017-01-15 21:36:46 -0800

    Add searchbar component

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = 'a2d3a07e8a17519a37544b98649a9504';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+
+export const FETCH_WEATHER = 'FETCH_WEATHER'
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  }
+}
